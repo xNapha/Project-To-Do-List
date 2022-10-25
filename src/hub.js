5f84d7db47fcaab82d6e05740b37f98379bd6d95
xNapha <nathancai1997@gmail.com>
2022-10-26 09:22:28 +1100

    Version 0.11
    Added due date and priority range indicator
    
    Added event to exit form when clicking on the darkened area, or exit when the add button is pressed

--- src/hub.js
+++ src/hub.js
@@ -3,8 +3,12 @@ import { } from "./index.js";
 const newCategoryForm = (x) =>{
     const hubFormCon = document.createElement("div");
     hubFormCon.setAttribute("id", "hubFormCon");
+    const hubFormExit = document.createElement("div");
+    hubFormExit.setAttribute("id","hubFormExit");
     const hubForm = document.createElement("form");
     hubForm.setAttribute("id", "hubForm");
+
+    closeFormEvent(hubFormExit).closeForm();
 
     const titleInputCon = document.createElement("div");
     const titleInput = document.createElement("input");
@@ -21,26 +25,64 @@ const newCategoryForm = (x) =>{
     description.setAttribute("placeholder", "Brief description...");
     descriptionCon.append(description);
 
+    const dueDateCon = document.createElement("div");
+    dueDateCon.setAttribute("id","dueDateCon");
+    const dueDateLabel = document.createElement("label");
+    dueDateLabel.setAttribute("id","dueDateLabel");
+    dueDateLabel.setAttribute("for","dueDate");
+    dueDateLabel.textContent = "Due: ";
+    const dueDate = document.createElement("input");
+    dueDate.setAttribute("id","dueDate");
+    dueDate.setAttribute("type","date");
+    dueDate.setAttribute("name","dueDate");
+    dueDateCon.append(dueDateLabel,dueDate);
+
+    const priorityCon = document.createElement("div");
+    priorityCon.setAttribute("id","priorityCon");
+    const priority = document.createElement("input");
+    priority.setAttribute("id","priority");
+    priority.setAttribute("type","range");
+    priority.setAttribute("min","1");
+    priority.setAttribute("value","3");
+    priority.setAttribute("max","5");
+    priorityCon.append(priority);
+
     const addButtonCon = document.createElement("div");
     const addButton = document.createElement("button");
     addButton.setAttribute("id","addButton");
     addButton.setAttribute("type","button");
     addButton.textContent = "Add new list";
-
-
-
     addButtonCon.append(addButton);
 
-    hubForm.append(titleInputCon, descriptionCon, addButtonCon);
-    hubFormCon.append(hubForm);
+    closeFormEvent(addButton).saveData();
+
+    hubForm.append(titleInputCon, descriptionCon, dueDateCon,priorityCon, addButtonCon);
+    hubFormCon.append(hubFormExit,hubForm);
     x.append(hubFormCon);
 }
 
-const categoryFormEvent = (area) =>{
+const closeFormEvent = (area) => {
+    const closeForm = () => 
+        area.addEventListener("click",()=>{
+            document.querySelector("#hubFormCon").remove()
+            document.querySelector("#contentCon").removeAttribute("class", "blur");
+        });
+    
+    const saveData = () =>{
+        area.addEventListener("click",() =>{
+            console.log("data saved");
+        });
+        closeForm(area);
+    };
 
-
-
+    
+    return { closeForm, saveData}
 }
+
+const saveData = (area) =>{
+    closeFormEvent(area);
+};
+
 
 
 export {newCategoryForm};
